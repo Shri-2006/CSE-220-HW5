54cb1cab2ad163c97422b2f9e946073db6de134a
Shriyans <shriyans.singh@stonybrook.edu>
2026-03-19 04:02:18 -0400

    starter code has been added

---
+++ matrix.c
@@ -0,0 +1,99 @@
+
+/*
+ * Homework 5 – Pointer Operations
+ * CSE 220 – Spring 2026
+ *
+ * Name:
+ * SBU ID:
+ */
+
+#include "matrix.h"
+#include <stdio.h>
+
+int max(int a, int b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+
+/*----------------------------------------------------
+  1. Sparse Matrix
+----------------------------------------------------*/
+int SparseMatrix(int D[2], int M[D[0]][D[1]],
+                 int S[3][(D[1] > D[0] ? D[1] : D[0])])
+{
+
+    int rows = D[0];
+    int cols = D[1];
+    int m = max(rows, cols);
+
+    int nonZeroCount = 0;
+
+    return 0;
+}
+
+
+/*----------------------------------------------------
+  2. Hadamard Product
+----------------------------------------------------*/
+int HadamardProduct(const int D[6],
+                    const int M[D[0]][D[1]],
+                    const int N[D[2]][D[3]],
+                    int A[D[4]][D[5]])
+{
+
+    int mRows = D[0];
+    int mCols = D[1];
+
+    int nRows = D[2];
+    int nCols = D[3];
+
+    int aRows = D[4];
+    int aCols = D[5];
+
+
+    return 0;
+}
+
+
+/*----------------------------------------------------
+  3. Matrix Multiplication
+----------------------------------------------------*/
+int Multiplication(const int D[6],
+                   const int M[D[0]][D[1]],
+                   const int N[D[2]][D[3]],
+                   int A[D[4]][D[5]])
+{
+
+    int mRows = D[0];
+    int mCols = D[1];
+
+    int nRows = D[2];
+    int nCols = D[3];
+
+    int aRows = D[4];
+    int aCols = D[5];
+
+
+    return 0;
+}
+
+
+/*----------------------------------------------------
+  4. Diagonal Sum
+----------------------------------------------------*/
+int DiagonalSum(const int D[4],
+                const int A[D[0]][D[1]],
+                int DS[D[2]][D[3]])
+{
+
+    int rows = D[0];
+    int cols = D[1];
+
+    int dsRows = D[2];
+    int dsCols = D[3];
+
+
+    return 0;
+}
